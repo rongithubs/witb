5dab9c4cf00f7b29239be4ae3b02bbc698c74cff
Ron Rivera <ronrivera@Rons-MacBook-Air.local>
2025-06-26 00:08:14 -0700

    clean up

--- witb-backend/main.py
+++ witb-backend/main.py
@@ -1,27 +1,18 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
-from schemas import WITBItem, WITBItemBase
-from sqlalchemy.future import select
 from sqlalchemy.exc import IntegrityError
-import models, schemas
-from database import SessionLocal, engine
-from sqlalchemy.exc import IntegrityError
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-from fastapi import Request
-import traceback
-from fastapi.middleware.cors import CORSMiddleware
 from typing import List
-from fastapi import Query
+import traceback
+import models, schemas
+from database import SessionLocal
 app = FastAPI()
-
-from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -36,7 +27,7 @@ async def get_db():
 
 @app.post("/players/", response_model=schemas.Player)
 async def create_player(player: schemas.PlayerCreate, db: AsyncSession = Depends(get_db)):
-    new_player = models.Player(**player.dict())
+    new_player = models.Player(**player.model_dump())
     db.add(new_player)
     try:
         await db.commit()
@@ -86,7 +77,7 @@ async def add_witb_item(
         if not player:
             raise HTTPException(status_code=404, detail="Player not found")
 
-        db_item = models.WITBItem(**item.dict(), player_id=player.id)
+        db_item = models.WITBItem(**item.model_dump(), player_id=player.id)
         db.add(db_item)
         await db.commit()
         await db.refresh(db_item)
